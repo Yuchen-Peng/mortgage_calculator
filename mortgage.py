597c5b6e01aa8b5bea76a00e0efa9d767b75c8f1
Yuchen Peng <pengpyc@gmail.com>
2017-12-31 10:54:18 -0500

    Update mortgage.py

--- mortgage.py
+++ mortgage.py
@@ -1,8 +1,18 @@
 class mortgage:
-	def __init__(self,price,rate,term):
+#	def __init__(self,price,rate,term):
+	def __init__(self):
+		price = float(input('Enter the house price:'))
+		rate = float(input('Enter the interest rate:'))
+		term = int(input('Enter the mortgage term in year:'))
 		self.price = price
 		self.rate = rate
 		self.term = term
+	def loan(self):
+		try:
+			ratio = float(input('Enter the downpayment ratio in percent:'))/100
+		except:
+			ratio = 0.2
+		return self.price * (1-ratio)
 	def month_pay(self):
 		a = 1
 		sum = 0
@@ -10,7 +20,7 @@ class mortgage:
 		for i in range(self.term*12):
 			sum += a
 			a = a/(1+r)
-		return self.price/sum
+		return self.loan()/sum
 	def __lt__(self,other):
 		return self.month_pay() > other.month_pay()
 
